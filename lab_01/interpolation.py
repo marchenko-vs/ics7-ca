7618d38ba576502fb977216074ef585cf10515f0
marchenko-vs <marchenko_vlse@mail.ru>
2022-03-04 22:33:40 +0300

    Hermite polynom functions were fixed.

--- lab_01/interpolation.py
+++ lab_01/interpolation.py
@@ -1,26 +1,25 @@
-def separate_function(x: list, y: list) -> float:
+EPS = 1e-3
+
+
+def print_table(table: dict):
+    print('Исходная таблица:')
+
+    print('-----------+-----------+------------')
+    print('     x     |     y     |     dy     ')
+    print('-----------+-----------+------------')
+
+    for i in range(len(table['x'])):
+        print('{:^11.6f}|{:^11.6f}|{:^11.6f}'.format(table['x'][i],
+                                                     table['y'][i],
+                                                     table['dy'][i]))
+        print('-----------+-----------+------------')
+
+
+def divided_difference_newton(x: list, y: list) -> float:
     if len(x) == 2:
         return (y[1] - y[0]) / (x[1] - x[0])
-    return (separate_function(x[:len(x) - 1], y[:len(x) - 1]) -
-            separate_function(x[1:], y[1:])) / (x[0] - x[len(x) - 1])
-
-
-def separate_function_2(x: list, y: list, dy: list) -> float:
-    if abs(x[1] - x[0]) < 1e-3:
-        return dy[0]
-    return separate_function(x, y)
-
-
-def interpolate_hermite(x: list, y: list, dy: list, degree: int,
-                        argument: float) -> float:
-    function = y[0]
-    for i in range(1, degree + 1):
-        multiplier = 1.0
-        for j in range(0, i):
-            multiplier *= argument - x[j]
-        multiplier *= separate_function_2(x[:i + 1], y[:i + 1], dy[:i + 1])
-        function += multiplier
-    return function
+    return (divided_difference_newton(x[:len(x) - 1], y[:len(x) - 1]) -
+            divided_difference_newton(x[1:], y[1:])) / (x[0] - x[len(x) - 1])
 
 
 def interpolate_newton(x: list, y: list, degree: int, argument: float) -> float:
@@ -29,9 +28,18 @@ def interpolate_newton(x: list, y: list, degree: int, argument: float) -> float:
         multiplier = 1.0
         for j in range(0, i):
             multiplier *= argument - x[j]
-        multiplier *= separate_function(x[:i + 1], y[:i + 1])
+        multiplier *= divided_difference_newton(x[:i + 1], y[:i + 1])
         function += multiplier
     return function
+
+
+def divided_difference_hermite(x: list, y: list, dy: list) -> float:
+    if len(x) == 2:
+        if abs(x[1] - x[0]) > EPS:
+            return (y[1] - y[0]) / (x[1] - x[0])
+        return dy[0]
+    return (divided_difference_hermite(x[:len(x) - 1], y[:len(x) - 1], dy[:len(x) - 1]) -
+           divided_difference_hermite(x[1:], y[1:], dy[1:])) / (x[0] - x[len(x) - 1])
 
 
 def interpolate_hermite(x: list, y: list, dy: list, nodes: int,
@@ -46,6 +54,6 @@ def interpolate_hermite(x: list, y: list, dy: list, nodes: int,
         multiplier = 1.0
         for j in range(0, i):
             multiplier *= argument - x[j]
-        multiplier *= separate_function_2(x[:i + 1], y[:i + 1], dy[:i + 1])
+        multiplier *= divided_difference_hermite(x[:i + 1], y[:i + 1], dy[:i + 1])
         function += multiplier
     return function
--- lab_01/main.py
+++ lab_01/main.py
@@ -1,24 +1,38 @@
-from interpolation import *
+import sys
+import interpolation as inpln
 
 
 def main():
+    try:
+        x = float(input('Введите аргумент функции: '))
+        n = int(input('Введите степень полинома: '))
+    except ValueError:
+        print('Аргумент должен быть вещественным числом.')
+        sys.exit(1)
+
     main_table = {'x': [0.0, 0.15, 0.3, 0.45, 0.6, 0.75, 0.9, 1.05],
                   'y': [1.0, 0.838771, 0.655336, 0.450447, 0.225336, -0.01831,
                         -0.27839, -0.55243],
                   'dy': [-1.0, -1.14944, -1.29552, -1.43497, -1.56464,
                          -1.68164, -1.78333, -1.86742]}
-    x = 0.525
+
+    inpln.print_table(main_table)
+
     print('-----------+-----------+-----------+-----------')
     print('     n     |     x     |   Newton  |  Hermite')
     print('-----------+-----------+-----------+-----------')
-    for i in range(1, 6):
+
+    for i in range(1, n + 1):
         print('{:^11d}|{:^11.6f}|{:^11.6f}|{:^11.6f}'.format(i, x,
-              interpolate_newton(main_table['x'], main_table['y'], i, x),
-              interpolate_hermite(main_table['x'].copy(), main_table['y'].copy(),
-                                  main_table['dy'].copy(), i, x)))
+              inpln.interpolate_newton(main_table['x'], main_table['y'], i, x),
+              inpln.interpolate_hermite(main_table['x'].copy(), 
+                                        main_table['y'].copy(),
+                                        main_table['dy'].copy(), i, x)))
         print('-----------+-----------+-----------+-----------')
-    print('Function root is {:.6f}.'.format(interpolate_newton(main_table['y'],
-          main_table['x'], 2, 0)))
+
+    print('Корень функции: {:.6f}.'.format(inpln.interpolate_newton(
+          main_table['y'],
+          main_table['x'], n, 0.0)))
 
 
 if __name__ == '__main__':
